be8b949e3ea70413d5295272b76f138ffb005047
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2020-10-03 04:10:01 +0300

    Update app.js

--- back/app.js
+++ back/app.js
@@ -14,13 +14,14 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.tokenExtractor)
 app.use(middleware.errorHandler)
-app.use(middleware.unknownEndpoint)
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
 app.use('/api/bikeData', bikeDataRouter)
+
+app.use(middleware.unknownEndpoint)
 
 const Logger = require('./utils/logger')
 console.log = Logger.consoleLogOverride
