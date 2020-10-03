a56e95b7beba506f0672027f74eef0af7f424c71
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2020-10-03 17:43:21 +0300

    build version of frontend

---
+++ back/build/precache-manifest.fe9cb46f0ce3098babd148002301762d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9197e3c4987875299042378a7b0c6e95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f181ebf2994187623cb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8cdfba0ac357be5b076",
+    "url": "/static/js/2.eae8d276.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eae8d276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f181ebf2994187623cb",
+    "url": "/static/js/main.0a5df48c.chunk.js"
+  },
+  {
+    "revision": "f9eb481a70f5f1617b9c",
+    "url": "/static/js/runtime-main.d640d062.js"
+  }
+]);
