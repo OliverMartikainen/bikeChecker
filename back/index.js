d79b2975152f04947ad6acfdde365b2becd20fdf
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2020-10-03 03:50:42 +0300

    Update index.js

--- back/index.js
+++ back/index.js
@@ -3,7 +3,8 @@ const config = require('./utils/config')
 const http = require('http') 
 
 const server = http.createServer(app)
+const nodeEnv = config.NODE_ENV.toUpperCase()
 
 server.listen(config.PORT, () => {
-    console.log(`Server version ${config.SERVER_VERSION} running from port ${config.PORT}`)
+    console.log(`${nodeEnv} server version ${config.SERVER_VERSION} running from port ${config.PORT}`)
 })
